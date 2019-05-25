b22a13dd20cf0a85d6450dca6619caa77367a931
DiegoPayan <dapayanl@concredito.com.mx>
2019-05-24 19:25:52 -0600

    Avance fallido del dia 2

--- Proyecto100/domino.hs
+++ Proyecto100/domino.hs
@@ -1,36 +1,54 @@
 import NumeroRandom
 
 -- Lista de las 28 fichas del juego
-fichas :: [[Integer]]
+fichas :: [[Int]]
 fichas = [ [x,y] | x<-[0..6] , y<-[x..6] ]
 
 -- Lista de randoms para seleccionar fichas para el jugador 1
-random1 :: [Int]
-random1 = [a,b,c,d,e,f,g]
+random1 :: [[Int]]
+random1 = [[a],[b],[c],[d],[e],[f],[g]]
 
 -- Lista de randoms para seleccionar fichas para el jugador 2
-random2 :: [Int]
-random2 = [h,i,j,k,l,m,n]
+random2 :: [[Int]]
+random2 = [[h],[i],[j],[k],[l],[m],[n]]
 
 -- Fichas del jugador 1
-jugador1 :: [Int]
+jugador1 :: [[Int]]
 jugador1 = []
 
 -- Fichas del jugador 2
-jugador2 :: [Int]
+jugador2 :: [[Int]]
 jugador2 = []
 
 -- Función principal del programa
 main = do
-        putStrLn "Inicia reparto de fichas"
-        let fichasUno = reparteFichas random1
-        let fichasDos = reparteFichas random2
-        print fichasUno
-        print fichasDos
-        putStrLn "Fin"
-         -- putStrLn . show =<< rand
+    putStrLn "Inicia reparto de fichas"
+    let spmt = func 0 random1
+    -- let fichasUno = reparteFichas random1
+    -- let fichasDos = reparteFichas random2
+    -- print fichasUno
+    -- print fichasDos
+    putStrLn "Fin"
+        -- putStrLn . show =<< rand
 
-reparteFichas rand = [ fichas!!xs | xs <- rand ]
+
+reparteFichas :: Int -> [[Int]] -> [[Int]]
+reparteFichas i rand = [  fichas!!xs  | xs <- rand!!i ] 
+
+func i xs =
+    if i < 7
+        then do
+            let ficha = reparteFichas i xs
+            let fichaRes = concat ficha 
+            let lista = sacaFicha fichaRes fichas
+            func (i+1) lista
+        else 
+            i
+
+sacaFicha :: [Int] -> [[Int]] -> [[Int]]
+sacaFicha _ [] = []
+sacaFicha x (y:ys)  | x ==  y = sacaFicha x ys
+                    | otherwise = y : sacaFicha x ys
         
 --Hacer un metodo toma y elimina, toma y elimina
 

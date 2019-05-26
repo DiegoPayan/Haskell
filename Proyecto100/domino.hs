7d1ef4fcb992540d1931f5f8529eb8d563f60aa6
DiegoPayan <dapayanl@concredito.com.mx>
2019-05-26 14:23:00 -0600

    Antes de Nico

--- Proyecto100/domino.hs
+++ Proyecto100/domino.hs
@@ -23,33 +23,30 @@ jugador2 = []
 -- Función principal del programa
 main = do
     putStrLn "Inicia reparto de fichas"
-    let spmt = func 0 random1
-    -- let fichasUno = reparteFichas random1
-    -- let fichasDos = reparteFichas random2
-    -- print fichasUno
-    -- print fichasDos
+    let sptm = func 0 random1
+    print sptm
     putStrLn "Fin"
         -- putStrLn . show =<< rand
 
 
 reparteFichas :: Int -> [[Int]] -> [[Int]]
-reparteFichas i rand = [  fichas!!xs  | xs <- rand!!i ] 
+reparteFichas i rand = [  fichas!!xs  | xs <- rand!!i ]
 
 func i xs =
     if i < 7
         then do
             let ficha = reparteFichas i xs
-            let fichaRes = concat ficha 
+            let fichaRes = concat ficha
             let lista = sacaFicha fichaRes fichas
             func (i+1) lista
-        else 
+        else
             i
 
 sacaFicha :: [Int] -> [[Int]] -> [[Int]]
 sacaFicha _ [] = []
 sacaFicha x (y:ys)  | x ==  y = sacaFicha x ys
                     | otherwise = y : sacaFicha x ys
-        
+
 --Hacer un metodo toma y elimina, toma y elimina
 
 {-
